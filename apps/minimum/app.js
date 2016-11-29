b57ea3ee9ffe18381988e291e9e97a1c5f8b9bbc
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-11-29 18:23:45 +0900

    TIFF-352 minimumアプリを調整。

--- apps/minimum/app.js
+++ apps/minimum/app.js
@@ -3,7 +3,34 @@ const express = require('express');
 // import cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // import multer = require('multer');
+const conf = require('config');
+const mongoose = require('mongoose');
+// let BearerStrategy = passportHttpBearer.Strategy;
+// passport.use(new BearerStrategy(
+//     (token, cb) => {
+//         Models.Authentication.findOne(
+//             {
+//                 token: token
+//             },
+//             (err, authentication) => {
+//                 if (err) return cb(err);
+//                 if (!authentication) return cb(null, false);
+//                 cb(null, authentication);
+//             }
+//         );
+//     }
+// ));
 let app = express();
+if (process.env.NODE_ENV !== 'prod') {
+    // サーバーエラーテスト
+    app.get('/minimum/500', (req, res) => {
+        req.on('data', (chunk) => {
+        });
+        req.on('end', () => {
+            throw new Error('500 manually.');
+        });
+    });
+}
 // view engine setup
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
@@ -13,6 +40,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // let storage = multer.memoryStorage()
 // app.use(multer({ storage: storage }).any());
 // app.use(cookieParser());
+// i18n を利用する設定
+// i18n.configure({
+//     locales: ['en', 'ja'],
+//     defaultLocale: 'en',
+//     directory: __dirname + '/../../locales',
+//     objectNotation: true,
+//     updateFiles: false // ページのビューで自動的に言語ファイルを更新しない
+// });
+// // i18n の設定を有効化
+// app.use(i18n.init);
+let MONGOLAB_URI = conf.get('mongolab_uri');
+mongoose.connect(MONGOLAB_URI, {});
+// if (process.env.NODE_ENV !== 'prod') {
+//     let db = mongoose.connection;
+//     db.on('connecting', () => {
+//         console.log('connecting');
+//     });
+//     db.on('error', (error) => {
+//         console.error('Error in MongoDb connection: ', error);
+//     });
+//     db.on('connected', () => {
+//         console.log('connected.');
+//     });
+//     db.once('open', () => {
+//         console.log('connection open.');
+//     });
+//     db.on('reconnected', () => {
+//         console.log('reconnected.');
+//     });
+//     db.on('disconnected', () => {
+//         console.log('disconnected.');
+//     });
+// }
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
 // import {useContainer, useExpressServer} from "routing-controllers";
--- apps/minimum/app.ts
+++ apps/minimum/app.ts
@@ -2,8 +2,44 @@ import express = require('express');
 // import cookieParser = require('cookie-parser');
 import bodyParser = require('body-parser');
 // import multer = require('multer');
+import conf = require('config');
+import mongoose = require('mongoose');
+// import i18n = require('i18n');
+// import passport = require('passport');
+// import passportHttpBearer = require('passport-http-bearer');
+import Models from '../common/models/Models';
+
+// let BearerStrategy = passportHttpBearer.Strategy;
+// passport.use(new BearerStrategy(
+//     (token, cb) => {
+//         Models.Authentication.findOne(
+//             {
+//                 token: token
+//             },
+//             (err, authentication) => {
+//                 if (err) return cb(err);
+//                 if (!authentication) return cb(null, false);
+
+//                 cb(null, authentication);
+//             }
+//         );
+//     }
+// ));
 
 let app = express();
+
+
+if (process.env.NODE_ENV !== 'prod') {
+    // サーバーエラーテスト
+    app.get('/minimum/500', (req, res) => {
+        req.on('data', (chunk) => {
+        });
+
+        req.on('end', () => {
+            throw new Error('500 manually.');
+        })
+    });
+}
 
 // view engine setup
 app.set('views', `${__dirname}/views`);
@@ -17,6 +53,50 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(multer({ storage: storage }).any());
 
 // app.use(cookieParser());
+
+
+
+
+
+// i18n を利用する設定
+// i18n.configure({
+//     locales: ['en', 'ja'],
+//     defaultLocale: 'en',
+//     directory: __dirname + '/../../locales',
+//     objectNotation: true,
+//     updateFiles: false // ページのビューで自動的に言語ファイルを更新しない
+// });
+// // i18n の設定を有効化
+// app.use(i18n.init);
+
+
+
+
+
+let MONGOLAB_URI = conf.get<string>('mongolab_uri');
+mongoose.connect(MONGOLAB_URI, {
+});
+// if (process.env.NODE_ENV !== 'prod') {
+//     let db = mongoose.connection;
+//     db.on('connecting', () => {
+//         console.log('connecting');
+//     });
+//     db.on('error', (error) => {
+//         console.error('Error in MongoDb connection: ', error);
+//     });
+//     db.on('connected', () => {
+//         console.log('connected.');
+//     });
+//     db.once('open', () => {
+//         console.log('connection open.');
+//     });
+//     db.on('reconnected', () => {
+//         console.log('reconnected.');
+//     });
+//     db.on('disconnected', () => {
+//         console.log('disconnected.');
+//     });
+// }
 
 import "reflect-metadata";
 import {useExpressServer} from "routing-controllers";
